5db3e52329f6b8c7a3dee18a7d6cb79e9b833035
shofi384@gmail.com Rahman <shofi384@gmail.com>
2018-07-10 16:51:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,11 @@
 function forLoop(a) {
   for(var i=0; i<25; i++) {
     if(i === 1) {
-      a.push()
+      a.push("I am 1 strange loop.");
+    }
+    else {
+      a.push("I am ${i} strange loops.");
     }
   }
+  return a;
 }
